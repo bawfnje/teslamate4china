a766dd0480097ee0aed95165d0d77305e138e558
陈伟明 <chenweiming@sinosoft.com.cn>
2022-12-13 21:15:51 +0800

    中英对照

--- restart.sh
+++ restart.sh
@@ -1,13 +1,11 @@
 # open -g -a "Docker Desktop.app"
 docker-compose stop
 echo "=========>docker-compose stop ==>over"
-docker rm teslamate4china_teslamate_1
 docker rm teslamate4china_teslamate4china_1
 docker rm teslamate4china_database_1
 docker rm teslamate4china_grafana_1
 docker rm teslamate4china_mosquitto_1
 echo "=========>docker rm teslamate4china_* ==>over"
-docker image rm teslamate4china_teslamate
 docker image rm teslamate4china_teslamate4china
 docker image rm teslamate4china_grafana
 echo "=========>docker image rm teslamate4china_teslamate4china ==>over"
@@ -19,4 +17,3 @@ echo "=========>docker image rm teslamate4china_teslamate4china ==>over"
 echo "=========>docker-compose up -d ==>start"
 docker-compose up -d
 open http://localhost:4000
-open http://localhost:4400
