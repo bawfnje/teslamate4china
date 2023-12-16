e1005379a830ffc89807d35f7813bcc7bb813b5d
陈伟明 <chenweiming@sinosoft.com.cn>
2023-12-16 11:49:03 +0800

    push.sh dockerbuild

--- push.sh
+++ push.sh
@@ -1,3 +1,9 @@
+# error: At least one invalid signature was encountered "至少遇到一个无效签名"相关问题及解决方法
+docker image prune -a
+docker container prune -a
+docker system prune
+docker system df
+
 docker login -u admin -p makelt8485 i.bawfnje.work:8000
 docker build . --pull -t i.bawfnje.work:8000/teslamate4china:latest
 docker push i.bawfnje.work:8000/teslamate4china:latest
