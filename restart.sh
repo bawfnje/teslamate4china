c158ed0fd8bb76cc4713e2fa6e2b2e905a043385
陈伟明 <chenweiming@sinosoft.com.cn>
2023-12-21 10:16:42 +0800

    中文汉化更新

--- restart.sh
+++ restart.sh
@@ -1,13 +1,13 @@
 # open -g -a "Docker Desktop.app"
 docker-compose stop
 echo "=========>docker-compose stop ==>over"
-docker rm teslamate4china_teslamate4china_1
-docker rm teslamate4china_database_1
-docker rm teslamate4china_grafana_1
-docker rm teslamate4china_mosquitto_1
+# docker rm teslamate4china-service
+# docker rm teslamate4china_database
+docker rm teslamate4china-grafana
+# docker rm teslamate4china_mosquitto
 echo "=========>docker rm teslamate4china_* ==>over"
-docker image rm teslamate4china_teslamate4china
-docker image rm teslamate4china_grafana
+# docker image rm teslamate4china-service
+docker image rm teslamate4china-grafana
 echo "=========>docker image rm teslamate4china_teslamate4china ==>over"
 # docker volume rm teslamate4china_teslamate-grafana-data
 # docker volume rm teslamate4china_teslamate-db
@@ -15,10 +15,10 @@ echo "=========>docker image rm teslamate4china_teslamate4china ==>over"
 # docker volume rm teslamate4china_mosquitto-data
 # echo "=========>docker volume rm teslamate4china_teslamate-grafana-data ==>over"
 echo "=========>docker-compose up -d ==>start"
-docker image prune -a
-docker container prune -a
-docker system prune
+# docker image prune -a
+# docker container prune -a
+# docker system prune
 docker system df
 docker-compose up -d
-open http://localhost:4000
+open -a "google chrome" http://localhost:4000
 docker-compose logs -f
