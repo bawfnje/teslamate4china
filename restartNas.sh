d150f4dfdc831db01011db22308f0cd22b49200a
陈伟明 <chenweiming@sinosoft.com.cn>
2024-08-15 14:15:56 +0800

    docker-compose down

--- restartNas.sh
+++ restartNas.sh
@@ -3,21 +3,12 @@ export PATH=$PATH:/usr/syno/sbin:/usr/syno/bin:/usr/local/sbin:/usr/local/bin
 echo "PATH: $PATH"
 echo "which docker:`which docker`"
 cd /volume1/docker/teslamate
-sudo docker-compose stop
+# sudo docker-compose stop
 echo "\n=========>docker-compose stop ==>over"
-sudo docker rm teslamate4china-service
-# sudo docker rm teslamate4china_database
-sudo docker rm teslamate4china-grafana
-# sudo docker rm teslamate4china_mosquitto
+sudo docker pull teslamate4china-service
+sudo docker pull teslamate4china-grafana
 echo "\n=========>docker rm teslamate4china_* ==>over"
-sudo docker image rm teslamate4china-teslamate4china
-sudo docker image rm teslamate4china-grafana
-sudo rm -rf ./data/teslamate-grafana-data
-echo "\n=========>docker image rm teslamate4china_teslamate4china ==>over"
-# sudo docker volume rm teslamate4china_teslamate-grafana-data
-# sudo docker volume rm teslamate4china_teslamate-db
-# sudo docker volume rm teslamate4china_mosquitto-conf
-# sudo docker volume rm teslamate4china_mosquitto-data
-# echo "=========>docker volume rm teslamate4china_teslamate-grafana-data ==>over"
-echo "\n=========>docker-compose up -d ==>start"
-sudo docker-compose up -d
+sudo docker-compose down teslamate4china-service
+sudo docker-compose up -d teslamate4china-service
+sudo docker-compose down teslamate4china-grafana
+sudo docker-compose up -d teslamate4china-grafana
