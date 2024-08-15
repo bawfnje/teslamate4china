33b7cb69aac092885dc502aa7387ac1fca4d69b3
陈伟明 <chenweiming@sinosoft.com.cn>
2024-08-15 14:33:50 +0800

    sudo

--- restartNas.sh
+++ restartNas.sh
@@ -3,14 +3,14 @@ export PATH=$PATH:/usr/syno/sbin:/usr/syno/bin:/usr/local/sbin:/usr/local/bin
 echo "PATH: $PATH"
 echo "which docker:`which docker`"
 cd /volume1/docker/teslamate
-echo "\n>>>login -u admin -p makelt8485 i.bawfnje.work:8000"
-docker login -u admin -p makelt8485 i.bawfnje.work:8000
-echo "\n=========>docker-compose pull new ==>over"
+echo ">>>login -u admin -p makelt8485 i.bawfnje.work:8000"
+sudo docker login -u admin -p makelt8485 i.bawfnje.work:8000
+echo "=========>docker-compose pull new ==>over"
 sudo docker-compose pull teslamate
 sudo docker-compose pull grafana
-echo "\n=========>docker restart teslamate ==>over"
+echo "=========>docker restart teslamate ==>over"
 sudo docker-compose down teslamate
 sudo docker-compose up -d teslamate
-echo "\n=========>docker-compose restart grafana ==>over"
+echo "=========>docker-compose restart grafana ==>over"
 sudo docker-compose down grafana
 sudo docker-compose up -d grafana
